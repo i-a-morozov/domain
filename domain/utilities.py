04e2135a230ee5d78c6e9bdf3fd81fa83b7b8fff
Ivan Morozov <ivan.morozov@elettra.eu>
2026-05-07 09:11:53 +0200

    05_06_2026: added moser map

--- domain/utilities.py
+++ domain/utilities.py
@@ -8,16 +8,6 @@ from numpy.typing import NDArray
 from numba import njit
 
 
-"""
-Utility mappings
-
-"""
-import numpy as np
-from numpy.typing import NDArray
-
-from numba import njit
-
-
 @njit
 def henon_forward(
     x: NDArray[np.float64],
@@ -152,3 +142,73 @@ def froeschle_inverse(
     px = ((px + 0.5) % 1.0) - 0.5
     py = ((py + 0.5) % 1.0) - 0.5
     return np.array([qx, qy, px, py])
+
+
+@njit
+def moser_forward(
+    x: NDArray[np.float64],
+    parameters: NDArray[np.float64]
+) -> NDArray[np.float64]:
+    """
+    4D Moser symplectic mapping.
+
+    Parameters
+    ----------
+    x: NDArray[np.float64], (dimension, )
+        Initial condition.
+    parameters: NDArray[np.float64]
+        Mapping parameters [a, b, c, alpha, beta, gamma, delta, mu]
+        C = [[alpha, beta], [gamma, delta]]
+        U(xi) = a*xi1 + b*xi2 + 0.5*c*xi1**2 + mu*xi1**3 + xi1*xi2**2
+
+    Returns
+    -------
+    NDArray[np.float64]
+
+    """
+    xi1, xi2, eta1, eta2 = x
+    a, b, c, alpha, beta, gamma, delta, mu = parameters
+    det = alpha*delta - beta*gamma
+    dU_dxi1 = a + c*xi1 + 3.0*mu*xi1**2 + xi2**2
+    dU_dxi2 = b + 2.0*xi1*xi2
+    rhs1 = -eta1 + alpha*xi1 + beta*xi2 + dU_dxi1
+    rhs2 = -eta2 + gamma*xi1 + delta*xi2 + dU_dxi2
+    xi1p = xi1 + (delta*rhs1 - gamma*rhs2)/det
+    xi2p = xi2 + (-beta*rhs1 + alpha*rhs2)/det
+    eta1p = alpha*xi1 + beta*xi2
+    eta2p = gamma*xi1 + delta*xi2
+    return np.array([xi1p, xi2p, eta1p, eta2p])
+
+
+@njit
+def moser_inverse(
+    x: NDArray[np.float64],
+    parameters: NDArray[np.float64]
+) -> NDArray[np.float64]:
+    """
+    4D Moser symplectic mapping inverse.
+
+    Parameters
+    ----------
+    x: NDArray[np.float64], (dimension, )
+        Initial condition.
+    parameters: NDArray[np.float64]
+        Mapping parameters [a, b, c, alpha, beta, gamma, delta, mu]
+        C = [[alpha, beta], [gamma, delta]]
+        U(xi) = a*xi1 + b*xi2 + 0.5*c*xi1**2 + mu*xi1**3 + xi1*xi2**2
+
+    Returns
+    -------
+    NDArray[np.float64]
+
+    """
+    xi1p, xi2p, eta1p, eta2p = x
+    a, b, c, alpha, beta, gamma, delta, mu = parameters
+    det = alpha*delta - beta*gamma
+    xi1 = (delta*eta1p - beta*eta2p)/det
+    xi2 = (-gamma*eta1p + alpha*eta2p)/det
+    dU_dxi1 = a + c*xi1 + 3.0*mu*xi1**2 + xi2**2
+    dU_dxi2 = b + 2.0*xi1*xi2
+    eta1 = eta1p + dU_dxi1 - alpha*(xi1p - xi1) - gamma*(xi2p - xi2)
+    eta2 = eta2p + dU_dxi2 - beta*(xi1p - xi1) - delta*(xi2p - xi2)
+    return np.array([xi1, xi2, eta1, eta2])
